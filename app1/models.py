d352c5b7f85f5ce5f37f4715ee7e46ff4df9a119
koki <s17161278@st.daito.ac.jp>
2022-06-08 01:36:42 +0900

    プロジェクトとモデルを追加

---
+++ app1/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Model1(models.Model):
+    attr1 = models.CharField(max_length=100)
+    attr2 = models.CharField(max_length=100)
+
+
+class Model2(models.Model):
+    attr1 = models.CharField(max_length=100)
+    attr2 = models.CharField(max_length=100)
